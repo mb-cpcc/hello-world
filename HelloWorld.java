7d090fb2e21232fa56ea107d33050fc17b1abd49
Melaniya Batsuk <mbatsuk0@email.cpcc.edu>
2025-09-18 11:49:50 -0400

    Revert "Update HelloWorld.java"
    This reverts commit e343b2712ddceee20ef4037a528c0c9f4df515ef.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,5 @@
 public class HelloWorld {
    public static void main(String[] args){
       System.out.println("Hello World!");
-      
-      String name = "George Washington";
-      
-      System.out.println(name);
-      
-      int age = 24;
-      
-      System.out.println(age);
-      
    }
 }
